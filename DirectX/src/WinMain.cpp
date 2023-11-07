a920aa6699455ebf834a5b45673e9ec43d7720d3
Jay Nakum <nakum.jay.m@gmail.com>
2023-11-07 11:50:32 +0530

    Creating a Window

---
+++ DirectX/src/WinMain.cpp
@@ -0,0 +1,44 @@
+#include <Windows.h>
+
+int CALLBACK WinMain(
+	HINSTANCE	hInstance,
+	HINSTANCE	hPrevInstance,
+	LPSTR		LpCmdLine,
+	int			nCmdShow
+)
+{
+	const auto pClassName = "WNDClass";
+
+	// register windows class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"Happy Hard Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		0, 0,
+		640, 480,
+		nullptr, nullptr,
+		hInstance,
+		nullptr
+	);
+	ShowWindow(hWnd, SW_SHOW);
+
+	while (true);
+
+	return 0;
+}
